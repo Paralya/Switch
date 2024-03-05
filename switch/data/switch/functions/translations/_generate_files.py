4a094f8917c8ce86cf50dc66bcdb44f72c065d4f
Stoupy51 <stoupy51@gmail.com>
2024-03-05 23:49:21 +0100

    Added title to the script

--- switch/data/switch/functions/translations/_generate_files.py
+++ switch/data/switch/functions/translations/_generate_files.py
@@ -1,5 +1,5 @@
 
-## Python script that will extract all tellraw messages in the functions folder and generate a file for easy translations possibility
+## Python script that will extract all tellraw and title messages in the functions folder and generate a file for easy translations possibility
 # Stop if the execution is not in the same folder as the script
 import os
 if os.getcwd() != os.path.dirname(os.path.realpath(__file__)):
@@ -10,34 +10,37 @@ if os.getcwd() != os.path.dirname(os.path.realpath(__file__)):
 # Constants and imports
 FUNCTIONS_FOLDER = "../"
 FOLDERS_TO_IGNORE = ["translations"]
+MESSAGE_TYPES = ["tellraw", "title"]
+SPLITTERS = [m_type + " @" for m_type in MESSAGE_TYPES]
 
 # Get each filepath where a tellraw is present and store the tellraw lines
-tellraws_per_file = dict()
+messages_per_file = dict()
 for root, dirs, files in os.walk(FUNCTIONS_FOLDER):
 	for file in files:
 		if file.endswith(".mcfunction") and not any(folder in root for folder in FOLDERS_TO_IGNORE):
 			path = os.path.join(root, file).replace("\\", "/")
 			with open(path, "r", encoding="utf-8") as f:
 				lines = f.readlines()
-				tellraws = []
+				messages = []
 				for line in lines:
-					if "tellraw @" in line:
-						tellraws.append(line)
-				if tellraws:
-					tellraws_per_file[path] = tellraws
+					for splitter in SPLITTERS:
+						if splitter in line:
+							messages.append((splitter, line))
+							break
+				if messages:
+					messages_per_file[path] = messages
 
 
-# For each path, create a file containing all tellraws
-SPLITTER = "tellraw @"
+# For each path, create a file containing all messages
 SCORE_SELECTOR = "switch.lang=0"
-for path, tellraws in tellraws_per_file.items():
+for path, messages in messages_per_file.items():
 	new_path = path.replace("../","").replace("/","_")
 	with open(new_path, "w", encoding="utf-8") as f:
 		content = "\n# French\n"
-		for line in tellraws:
+		for splitter, line in messages:
 
 			# Update the tellraw part to only select player with score of switch.lang equal to 0
-			splitted = line.split(SPLITTER)
+			splitted = line.split(splitter)
 			if splitted[1][1] != '[': # Do not have an advanced selector
 				splitted[1] = splitted[1][0] + f"[scores={{{SCORE_SELECTOR}}}]" + splitted[1][1:]
 			else: # Have an advanced selector
@@ -45,7 +48,7 @@ for path, tellraws in tellraws_per_file.items():
 					splitted[1] = splitted[1].replace("scores={", f"scores={{{SCORE_SELECTOR},")
 				else: # No score selector
 					splitted[1] = splitted[1][0] + f"[scores={{{SCORE_SELECTOR}}}," + splitted[1][2:]
-			line = SPLITTER.join(splitted)
+			line = splitter.join(splitted)
 
 			# Add the line to the file
 			content += line
@@ -55,7 +58,7 @@ for path, tellraws in tellraws_per_file.items():
 	# Update original file to aim for the new function
 	with open(path, "r", encoding="utf-8") as f:
 
-		# Get file content without lines containing tellraws
+		# Get file content without lines containing messages
 		new_content = []
 		lines = f.readlines()
 		last_line = 0
@@ -63,10 +66,10 @@ for path, tellraws in tellraws_per_file.items():
 			if "tellraw @" not in line:
 				new_content.append(line)
 			else:
-				last_line = i + 1 - len(tellraws) # As i increase even if the line is not written, we need to remove the number of lines we skip
+				last_line = i + 1 - len(messages) # As i increase even if the line is not written, we need to remove the number of lines we skip
 
 		# Add a function call to the new file
-		call = "function switch:translations/" + new_path.replace(".mcfunction","") + "\n"
+		call = "function switch:translations/tellraw/" + new_path.replace(".mcfunction","") + "\n"
 		new_content = new_content[:last_line] + [call] + new_content[last_line:]
 		new_content = "".join(new_content)
 	with open(path, "w", encoding="utf-8") as f:
