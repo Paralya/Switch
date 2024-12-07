072c1df40d31111fb0a399bd5db43ecd821fa923
Stoupy51 <stoupy51@gmail.com>
2024-12-07 14:22:10 +0100

    fix: 🚑 Fixed resource pack

--- user/database/misc_items.py
+++ user/database/misc_items.py
@@ -10,12 +10,13 @@ def setup_misc_items(database: dict[str, dict]) -> None:
 		database (dict[str, dict]): The database to update
 	"""
 	database["awakened_stardust_bow"] = {"id": "minecraft:bow"}
-	database["awakened_stardust_sniper"] = {"id": CUSTOM_ITEM_VANILLA}
+	database["awakened_stardust_sniper"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
 	database["awakened_stardust_block"] = {"id": CUSTOM_BLOCK_VANILLA, VANILLA_BLOCK: {"id": "minecraft:stone", "apply_facing": False}}
 	database["stardust_bow"] = {"id": "minecraft:bow"}
-	database["stardust_sniper"] = {"id": CUSTOM_ITEM_VANILLA}
+	database["stardust_sniper"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
 	database["ultimate_bow"] = {"id": "minecraft:bow"}
-	database["ultimate_sniper"] = {"id": CUSTOM_ITEM_VANILLA}
+	database["ultimate_sniper"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
+	database["ultimate_elytra"] = {"id": "minecraft:elytra"}
 	database["stardust_fragment"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["stardust_essence"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["stardust_config"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "block/command_block"}}
@@ -40,20 +41,20 @@ def setup_misc_items(database: dict[str, dict]) -> None:
 	}
 	database["dog_excrement"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["quarry_configurator"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["travel_staff"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["wormhole_potion"] = {"id": CUSTOM_ITEM_VANILLA}
+	database["travel_staff"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
+	database["wormhole_potion"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
 	database["banana"] = {"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":5, "saturation":6}}
-	database["anti_radiation_glove"] = {"id": CUSTOM_ITEM_VANILLA}
+	database["anti_radiation_glove"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
 	database["californium_core"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["californium_waste"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["radioactive_waste"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["radioactive_waste_barrel"] = {"id": CUSTOM_BLOCK_VANILLA, VANILLA_BLOCK: {"id":"minecraft:emerald_block", "apply_facing": False}}
 	database["uranium_shard"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["chargeless_wand"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["fertilizer_wand"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["fireball_wand"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["shield_wand"] = {"id": CUSTOM_ITEM_VANILLA}
-	database["storm_wand"] = {"id": CUSTOM_ITEM_VANILLA}
+	database["chargeless_wand"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
+	database["fertilizer_wand"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
+	database["fireball_wand"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
+	database["shield_wand"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
+	database["storm_wand"] = {"id": CUSTOM_ITEM_VANILLA, OVERRIDE_MODEL: {"parent": "minecraft:item/handheld"}}
 	database["souls_bag"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["souls_bag_half_filled"] = {"id": CUSTOM_ITEM_VANILLA}
 	database["souls_bag_filled"] = {"id": CUSTOM_ITEM_VANILLA}
