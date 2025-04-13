40e9cf7b6d8d5a7008a2268db0a0e32052a8ad43
Stoupy51 <stoupy51@gmail.com>
2025-04-13 22:31:53 +0200

    fix: 🚑 Updated python_datapack to fix the merge folder big issue

---
+++ build/datapack/data/switch/function/translations/_translate_fr_to_en.py
@@ -0,0 +1,57 @@
+
+## Python script that will try to translate all french message to english using a Translator API
+# Stop if the execution is not in the same folder as the script
+import os
+if os.getcwd() != os.path.dirname(os.path.realpath(__file__)):
+	print("Please execute this script in the same folder as the script")
+	exit()
+	pass
+
+# Constants and imports
+import deepl
+import time
+source = ("French", "fr", "switch.lang=0")
+target = ("English", "en-us", "switch.lang=1")
+auth_key = ""
+with open("_deepl_auth_key.txt", "r") as f: # "import deepl" ":fx"
+	auth_key = f.read().strip()
+	pass
+translator = deepl.Translator(auth_key)
+
+# For each file, translate the source messages to english
+start_time = time.time()
+for root, dirs, files in os.walk("./"):
+	for file in files:
+		if file.endswith(".mcfunction"):
+			path = os.path.join(root, file).replace("\\", "/")
+			with open(path, "r", encoding="utf-8") as f:
+				content = f.read()
+				if f"# {target[0]}" in content:
+					continue
+
+				# Get the french messages
+				source_messages = ""
+				langs = content.split("\n# ")
+				for language in langs:
+					if language.startswith(source[0]):
+						source_messages = "\n".join(language.split("\n")[1:])
+						break
+				
+				# Translate the french messages to english
+				translated_messages = ""
+				if source_messages:
+					translation = translator.translate_text(source_messages, source_lang=source[1], target_lang=target[1])
+					translated_messages = translation.text.replace('", "', '","').replace('": "', '":"')
+				pass
+
+			# Write the english messages to the file
+			with open(path, "w", encoding="utf-8") as f:
+				f.write(content + f"# {target[0]}\n" + translated_messages.replace(source[2], target[2]))
+				print(f"Translation done for {path}")
+			pass
+		pass
+	pass
+
+# Print final time
+print(f"Finished translating in {round(time.time() - start_time, 3)}s")
+
