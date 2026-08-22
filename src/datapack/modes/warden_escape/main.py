2cf5210555329c6481711ef7594815a125cee59e
Stoupy51 <stoupy51@gmail.com>
2026-08-22 15:17:32 +0200

    feat(warden_escape): ✨ Araignées à 2 PV

--- src/datapack/modes/warden_escape/main.py
+++ src/datapack/modes/warden_escape/main.py
@@ -104,7 +104,7 @@ execute if data storage {ns}:main {{map:"warden_escape_statue"}} as @e[limit=2] 
 execute if data storage {ns}:main {{map:"warden_escape_statue"}} as @e[limit=2] run summon warden 69033 126 69027
 
 # Summon one small spider per new warden also
-execute at @e[type=warden,tag=!{ns}.glowed] run summon spider ~ ~ ~ {{attributes:[{{id:"minecraft:scale",base:0.35}}]}}
+execute at @e[type=warden,tag=!{ns}.glowed] run summon spider ~ ~ ~ {{attributes:[{{id:"minecraft:scale",base:0.35}},{{id:"minecraft:max_health",base:2}}],Health:2f}}
 
 # Make the new wardens glow 2 seconds
 effect give @e[type=warden,tag=!{ns}.glowed] glowing 2 255 true
